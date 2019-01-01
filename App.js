c1420dda87f891692db24f2b63ecb760829ceb62
IOS <>
2019-01-01 10:40:20 +0530

    first commit

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, AsyncStorage} from 'react-native';
 import Parse from 'parse/react-native';
 
 export default class App extends Component{
